ad5e795e9a51789e4713e5002e4190d008970e40
JuanDRSoft <juan.rios@celuweb.com>
2024-10-11 14:11:56 -0500

    feat/ active date en calendario

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,23 +1,27 @@
-import { useState } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react'
 import SelectMonth from './components/SelectMonth'
+
+const data = [{ date: '2024-10-04 03:00:00' }, { date: '2024-09-04 03:00:00' }]
+const months = [
+  '',
+  'Enero',
+  'Febrero',
+  'Marzo',
+  'Abril',
+  'Mayo',
+  'Junio',
+  'Julio',
+  'Agosto',
+  'Septiembre',
+  'Octubre',
+  'Noviembre',
+  'Diciembre'
+]
 
 const Calendar = () => {
   const [currentDate, setCurrentDate] = useState(new Date())
-  const months = [
-    '',
-    'Enero',
-    'Febrero',
-    'Marzo',
-    'Abril',
-    'Mayo',
-    'Junio',
-    'Julio',
-    'Agosto',
-    'Septiembre',
-    'Octubre',
-    'Noviembre',
-    'Diciembre'
-  ]
+  const [activeDates, setActiveDates] = useState([])
 
   function getDaysInMonth(year, month) {
     return new Date(year, month + 1, 0).getDate() // El último día del mes
@@ -41,6 +45,21 @@ const Calendar = () => {
   for (let day = 1; day <= daysInMonth; day++) {
     daysArray.push(day)
   }
+
+  useEffect(() => {
+    if (data.length) {
+      const array = []
+      for (let i = 0; i < data.length; i++) {
+        const dateMonth = new Date(data[i].date).getMonth()
+        const dateYear = new Date(data[i].date).getFullYear()
+        if (dateMonth == month && dateYear == year) {
+          array.push(data[i])
+        }
+      }
+      setActiveDates(array)
+    }
+  }, [data, month, year])
+
   return (
     <div className="grid gap-3">
       <SelectMonth setCurrentDate={setCurrentDate} months={months} currentDate={currentDate} />
@@ -52,16 +71,20 @@ const Calendar = () => {
           </div>
         ))}
 
-        {daysArray.map((day, index) => (
-          <div
-            key={index}
-            className={`${
-              !day && 'bg-white'
-            } bg-gray-100 w-10 h-10 text-xl rounded-full flex justify-center items-center`}
-          >
-            {day || ''}
-          </div>
-        ))}
+        {daysArray.map((day, index) => {
+          const find = activeDates.find((e) => Number(new Date(e.date).getDate()) == day)
+          return (
+            <div
+              key={index}
+              className={`${find && '!bg-blue-500 text-white'}
+                ${
+                  !day && 'bg-white'
+                }  bg-gray-100 w-10 h-10 text-xl rounded-full flex justify-center items-center`}
+            >
+              {day || ''}
+            </div>
+          )
+        })}
       </div>
     </div>
   )
