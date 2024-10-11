ad46de01b3a32692fd1929ea5f0cfdba59555652
JuanDRSoft <juan.rios@celuweb.com>
2024-10-11 12:41:31 -0500

    fix/ tamaño en fechas

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -47,7 +47,7 @@ const Calendar = () => {
 
       <div className="grid grid-cols-7 gap-2 overflow-hidden p-5 bg-white rounded-3xl">
         {['D', 'L', 'M', 'M', 'J', 'V', 'S'].map((dayName) => (
-          <div className="flex justify-center items-center uppercase text-xl w-12" key={dayName}>
+          <div className="flex justify-center items-center uppercase text-xl w-10" key={dayName}>
             {dayName}
           </div>
         ))}
@@ -57,7 +57,7 @@ const Calendar = () => {
             key={index}
             className={`${
               !day && 'bg-white'
-            } bg-gray-100 w-12 h-12 text-xl rounded-full flex justify-center items-center`}
+            } bg-gray-100 w-10 h-10 text-xl rounded-full flex justify-center items-center`}
           >
             {day || ''}
           </div>
